34ed3f89392228397fafa78ef8566e312d3af273
bambangsetyawan <bams@bambangsetyawan.com>
2017-11-16 17:38:03 +0800

    Update header.php
    delete google analityc tags

--- header.php
+++ header.php
@@ -17,16 +17,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109427552-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-109427552-1');
-    </script>
-
 </head>
 
 <body <?php body_class(); ?>>
